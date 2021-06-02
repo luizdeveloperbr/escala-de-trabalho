e898e27f8aa3645873762a2dc23006577a23edf4
Eduardo Souza <l.eduardos.lopes@gmail.com>
2021-06-02 11:34:54 +0000

    Initial

---
+++ escala/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      texto: 'Escala',
+    };
+  },
+};
+</script>
+
+<template>
+  <div :class="$style.App">
+    <h1>Bem Vindo ao App {{texto}}</h1>
+  </div>
+</template>
+
+<style module>
+  .App {
+    padding: 20px;
+  }
+</style>
