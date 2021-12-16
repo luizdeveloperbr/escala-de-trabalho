262d37cf3d34b87ae8a9de0ee63ef03fe3232425
Eduardo Souza <l.eduardos.lopes@gmail.com>
2021-12-16 19:38:29 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -5,9 +5,13 @@ import 'firebase/compat/database'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-  databaseURL: "https://escala-lider.firebaseio.com",
-  storageBucket: "escala-lider.appspot.com",
-  apiKey: "AIzaSyAF0eprbhSc4BnDAgukV7XvyQB6VZbY8Zg",
+  apiKey: "AIzaSyCKJ5IMfVYYncyZi6lMU_tk26eZ3LkeL8w",
+  authDomain: "back-end-cf6f5.firebaseapp.com",
+  databaseURL: "https://back-end-cf6f5.firebaseio.com",
+  projectId: "back-end-cf6f5",
+  storageBucket: "back-end-cf6f5.appspot.com",
+  messagingSenderId: "326153185342",
+  appId: "1:326153185342:web:a7258509755b623147896f"
 };
 
 export const db = firebase.initializeApp(firebaseConfig).database()
